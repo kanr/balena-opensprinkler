36d7b389cb86d46c766664072dda7fdd21fb7f22
Connor <connor.aitken@gmail.com>
2018-11-30 23:31:00 -0800

    exapnding to catch the data from sender

--- nodered/app/nodes/node-red-contrib-opensprinkler/getControllerVariables/getControllerVariables.js
+++ nodered/app/nodes/node-red-contrib-opensprinkler/getControllerVariables/getControllerVariables.js
@@ -8,16 +8,19 @@ module.exports = function(RED) {
            request.post({
                url: process.env.OPENSPRINKLER_ADDRESS + '/jc?pw=' + process.env.OPENSPRINKLER_PASSWORD
            }, function(error, response, body){
-               if (error) {
-                   return node.error("an error occurred: " + error);
+               if (!error && response.statusCode == 200) {
+                   body = JSON.parse(body);
+                   msg = {
+                       payload: body 
+                   };
+                   node.send(msg);
+                    
+               }else{
+                   node.error("an error occurred: " + error);
                }
-               msg = {
-                   payload: response.statusCode
-               };
-               node.send(msg);
-           })
+           });
 
         });
     }
     RED.nodes.registerType("OpenSprinkler",getControllerVariables);
-}
+};
