ed54b12d3ecbcff4ba0332eef236a284c6a4f3bb
Connor <connor.aitken@gmail.com>
2018-12-01 00:25:37 -0800

    first commit of second node

---
+++ nodered/app/nodes/node-red-contrib-opensprinkler/getOptions/getOptions.js
@@ -0,0 +1,26 @@
+var request = require('request');
+var msg;
+module.exports = function(RED) {
+    function getControllerOptions(config){
+        RED.nodes.createNode(this,config);
+        var node = this;
+        this.on('input', function(msg) {
+           request.post({
+               url: process.env.OPENSPRINKLER_ADDRESS + '/jo?pw=' + process.env.OPENSPRINKLER_PASSWORD
+           }, function(error, response, body){
+               if (!error && response.statusCode == 200) {
+                   body = JSON.parse(body);
+                   msg = {
+                       payload: body 
+                   };
+                   node.send(msg);
+                    
+               }else{
+                   node.error("an error occurred: " + error);
+               }
+           });
+
+        });
+    }
+    RED.nodes.registerType("getOptions",getControllerOptions);
+};
